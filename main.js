4a0686fe1608cc0fe798ea7fffd2d3493f8b7baf
Lucas <lucasrustacean@gmail.com>
2024-04-03 09:33:11 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { Spot, WebsocketStream } = require('@binance/connector')
+
+const apiKey = process.env.API_KEY
+const apiSecret = process.env.API_SECRET
+const targetCoin = process.env.TARGET_COIN.toLowerCase()
+const investment = process.env.INV
+
+const client = new Spot(apiKey, apiSecret)
+
+const callbacks = {
+    open: () => {
+        console.log('Connected with Websocket server')
+    },
+    close: () => {
+        console.log('Disconnected with Websocket server')
+    },
+    message: data => {
+        console.log(data)
+        client.newOrder(targetCoin, 'BUY', 'MARKET', {
+            quoteOrderQty: investment,
+          }).then(response => {
+            client.logger.log(response.data)
+        })
+        .catch(error => client.logger.error(error))
+        .finally(
+            executed = true
+        )
+    }
+}  
+
+const stream = new WebsocketStream({callbacks})
+
+stream.bookTicker(targetCoin)
